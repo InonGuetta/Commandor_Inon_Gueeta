aa23c03c1279fc3a39f1be9e258469c8daf862b4
Inon Guetta <inon16guetta@gmail.com>
2025-05-27 11:17:50 +0300

    commit two

--- Commandor/Classes/Commando.cs
+++ Commandor/Classes/Commando.cs
@@ -1,19 +1,35 @@
-﻿public class Commando
+﻿using System;
+
+public class Commando
 {
     public string Name;
     public string CodeName;
     public string[] Tools = { "hammer", "rope", "chisel", "bag", "water buttle'" };
-    public string CodeName;
+    public string Status;
 
 
     // עכשיו בכל יצירת instance הוא יהיה חייב להיות מאותחל כמו כאן ב 
-    public Commando(string name, string codeName,)
+    public Commando(string name, string codeName, string status)
     {
-
+        this.Name = name;
+        this.CodeName = codeName;
+        this.Status = status;
 
     }
 
-
+    public void Walk()
+    {
+        Status = "Walk";
+    }
+      public void Hide()
+    {
+        Status = "Hide";
+    }
+      
+    public void Attack()
+    {
+        Console.WriteLine($"the soldier {CodeName} is attack");
+    }
 
 
 }
