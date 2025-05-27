cdcc05cc9a716df5e4fce36490a4e1740f3d9029
Inon Guetta <inon16guetta@gmail.com>
2025-05-27 10:28:57 +0300

    class comando include constractor

---
+++ Commandor/Classes/Commando.cs
@@ -0,0 +1,19 @@
+﻿public class Commando
+{
+    public string Name;
+    public string CodeName;
+    public string[] Tools = { "hammer", "rope", "chisel", "bag", "water buttle'" };
+    public string CodeName;
+
+
+    // עכשיו בכל יצירת instance הוא יהיה חייב להיות מאותחל כמו כאן ב 
+    public Commando(string name, string codeName,)
+    {
+
+
+    }
+
+
+
+
+}
